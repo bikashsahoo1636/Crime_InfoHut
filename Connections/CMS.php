ee35eb9d3638b95d684e7db839c1a27f606d6f2b
bikashsahoo1636 <bikashsahoo779@gmail.com>
2020-05-25 21:44:32 +0530

    added the documentation

--- Connections/CMS.php
+++ Connections/CMS.php
@@ -4,8 +4,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_CMS = "localhost";
-$database_CMS = "cms";
-$username_CMS = "root";
-$password_CMS = "";
+$database_CMS = "id13839881_cms";
+$username_CMS = "id13839881_crime";
+$password_CMS = "5&k23zU-SoHleYF#";
 $CMS = mysqli_connect($hostname_CMS, $username_CMS, $password_CMS,$database_CMS) or trigger_error(mysqli_connect_error(), E_USER_ERROR);
 ?>
