1dccdb0441236a9e3f2b277a250c75709477b80b
Bikash Sahoo <bikashsahoo779@gmail.com>
2020-05-25 18:35:23 +0530

    Add files via upload

---
+++ PoliceStation/Footer.php
@@ -0,0 +1,15 @@
+<div id="templatemo_content_bottom"></div>
+
+
+<div id="templatemo_footer">
+    <ul class="footer_menu">
+        <li><a href="index.php" class="current">Home</a></li>
+        <li><a href="Profile.php">Profile</a></li>
+        <li><a href="Complains.php">Complaints</a></li>
+        <li><a href="Missing.php">Missing Persons</a></li>
+        <li><a href="Wanted.php">Most Wanted</a></li>
+        <li><a href="Logout.php">Logout</a></li>
+    </ul>
+
+    Copyright © 2020 Crime InfoHut</div> 
+<!-- end of footer -->
